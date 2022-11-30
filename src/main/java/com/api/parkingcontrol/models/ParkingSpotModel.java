bcd59d53fc690f43da8e3c8fc19b912780afe0c2
MotaNT <natasha.silva4@fatec.sp.gov.br>
2022-11-30 17:01:07 -0300

    model class add

---
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,38 @@
+package com.api.parkingcontrol.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "SPT_PARKINGSPOTS")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+    @Column(nullable = false)
+    private LocalDateTime registrationData;
+    @Column(nullable = false, length = 150)
+    private String responsibleName;
+    @Column(nullable = false, length = 30)
+    private String apartament;
+    @Column(nullable = false, length = 30)
+    private String block;
+
+}
